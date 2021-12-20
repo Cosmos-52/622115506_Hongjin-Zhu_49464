93d9c46fc156a9a9a1f513b0cbf729b2264c7ac3
Cosmos-52 <hongjin_zhu@cmu.ac.th>
2021-12-20 22:07:52 +0800

    Activity3 SecondApp 2.1

--- secondapp/lib/pages/home.dart
+++ secondapp/lib/pages/home.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:secondapp/pages/detail.dart';
 
@@ -15,40 +17,27 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text("Computer Knowledge"),
       ),
-      body: ListView(
-        children: [
-          MyBox(
-            "Who I am?",
-            "May take my whole life to think about it",
-            "https://cdn.pixabay.com/photo/2021/10/12/04/00/frost-6702335_1280.jpg"
-          ),
-          SizedBox(height: 15,),
-          MyBox(
-            "Am I real?",
-            "If I could be in someone else's dream",
-            "https://cdn.pixabay.com/photo/2020/07/24/17/24/meadow-5434588__340.jpg"
-          ),
-          SizedBox(height: 15,),
-          MyBox(
-            "what make creature can think, can love, can cry for other?",
-            "cell...but I prefer the soul is true",
-            "https://cdn.pixabay.com/photo/2017/12/11/18/13/christmas-3012862__480.jpg"
-          ),
-          SizedBox(height: 15,),
-          TextButton(
-            onPressed: (){
-              print("next page >>");
-              Navigator.push(context, MaterialPageRoute(builder:  (context)=>DetailsPage()));
-            }, 
-            child: Text("read more")
-          )
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: FutureBuilder(
+          builder: (context, snapshot){
+            var data = json.decode(snapshot.data.toString());
+            return ListView.builder(
+              itemBuilder: (BuildContext context, int index){
+              return MyBox(data[index]['title'], data[index]['subtitle'], data[index]['image_url']);
+            },
+            itemCount: data.length,
+          );
+          },
+          future: DefaultAssetBundle.of(context).loadString('assets/data.json'),
+        )
       ),
     );
   }
 
   Widget MyBox(String title, String subtitle, String imagelink){
     return Container(
+      margin: EdgeInsets.only(top: 8),
       padding: EdgeInsets.all(20),
       height: 150,
       decoration: BoxDecoration(
